56815f1b553f4aa76d90acfa29464fa294d4acfa
mhadziqrazin <mhadziq0403@gmail.com>
2025-06-25 12:03:46 +0700

    feat: init views and urls

--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductView(APIView):
+    def get(self, request):
+        MOCK_DATA = [
+            { 'price': 100, 'name': 'Produk 1' },
+            { 'price': 200, 'name': 'Produk 2' },
+            { 'price': 300, 'name': 'Produk 3' },
+        ]
+        return Response(ProductSerializer(data, many=True).data)
