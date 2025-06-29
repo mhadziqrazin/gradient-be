370ccc5fc0d9dd879227121f32d33c3371bc8d32
mhadziqrazin <mhadziq0403@gmail.com>
2025-06-29 13:05:54 +0700

    refactor: remove unnecessary model

--- product/models.py
+++ product/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
-class Product(models.Model):
-    id = models.IntegerField(primary_key=True)
-    price = models.IntegerField()
-    name = models.CharField()
-
-    def __str__(self):
-        return self.name
+# Create your models here.
